e38a247442ada9f191e8ad33e84cdb59d6ab670e
Anuj Singhal <singhalanuj7575@gmail.com>
2025-09-13 16:01:00 +0530

    Keeping only prod files

--- budget-buddy/src/dashboards/index.js
+++
@@ -1,3 +0,0 @@
-export { default as DashboardContainer } from './DashboardContainer';
-export { default as DashboardSelector } from './DashboardSelector';
-export { dashboardConfigs, getDashboardConfig } from './dashboardConfig';
